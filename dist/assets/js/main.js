8268b93bc7ffbcb3705aba06e692435037841b84
Jason Spiller <jason@thetargetlab.com>
2018-08-16 17:52:59 -0600

    Interface almost complete.

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,1 +1,22 @@
 console.log('Hello Dave.');
+
+
+// jquery ready
+$(function(){
+
+  var bedroomSliderValue = document.getElementById("bedroomSlider");
+  var bathroomSliderValue = document.getElementById("bathroomSlider");
+  bedrooms.innerHTML = bedroomSliderValue.value;
+  bathrooms.innerHTML = bathroomSliderValue.value;
+  
+
+  // update room values
+  bedroomSlider.oninput = function() {
+    bedrooms.innerHTML = bedroomSliderValue.value;
+  }
+
+  bathroomSlider.oninput = function() {
+    bathrooms.innerHTML = bathroomSliderValue.value;
+  }
+
+});
